3fdb00258fa25dddb3056f338105715129fb7fac
braulio1309 <brauliozapatad@gmail.com>
2021-11-15 15:49:33 -0400

    inicio de servidor UDP

--- BuscarArchivos.c
+++ BuscarArchivos.c
@@ -1,4 +1,6 @@
+#include <pthread.h>
 #include "ArchivosLibrerias.h"
+#include "Servidores.h"
 
 
 int main(){
@@ -7,11 +9,18 @@ int main(){
 	char encontrar[120];
 	char an[100];
 	char path[100];
+/*	pthread_t proceso1;
+	pthread_t proceso2;*/
 	strcpy(inicio, "./");
 //	strcpy(encontrar, "texto");
-encontrar[0] = '\0';
-vaciarCadena(encontrar);
-	procesoArchivo(inicio, encontrar, an);
+	encontrar[0] = '\0';
+	vaciarCadena(encontrar);
+	/*pthread_create(&proceso1, NULL, &procesoArchivo, inicio, encontrar, an);
+	pthread_create(&proceso2, NULL, &procesoArchivo, "./Dentro", encontrar, an);
+	pthread_join(proceso1, NULL);
+	pthread_join(proceso2, NULL);*/
+	//procesoArchivo(inicio, encontrar, an);
+	server();
 
   return 0;
 }
